333dbdb05768c1fd4525492975d561095039fd42
SLylova <s.lylova@g.nsu.ru>
2025-12-06 20:16:18 +0700

    make shorter

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -12,26 +12,22 @@
  */
 #pragma once
 
-#include "../various/BumpAllocator.h"
-
 const int inf = 1e9;
 struct Node {
 	Node *l = 0, *r = 0;
 	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
+	Node(int lo,int hi):lo(lo),hi(hi){} // interval for [lo, hi)
 	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
+		if (hi - lo > 1) {
+			int mid = (lo + hi)/2;
 			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
 			val = max(l->val, r->val);
-		}
-		else val = v[lo];
+		} else val = v[lo];
 	}
 	int query(int L, int R) {
 		if (R <= lo || hi <= L) return -inf;
 		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
+		push();	return max(l->query(L, R), r->query(L, R));
 	}
 	void set(int L, int R, int x) {
 		if (R <= lo || hi <= L) return;
@@ -47,15 +43,14 @@ struct Node {
 			if (mset != inf) mset += x;
 			else madd += x;
 			val += x;
-		}
-		else {
+		} else {
 			push(), l->add(L, R, x), r->add(L, R, x);
 			val = max(l->val, r->val);
 		}
 	}
 	void push() {
 		if (!l) {
-			int mid = lo + (hi - lo)/2;
+			int mid = (lo + hi)/2;
 			l = new Node(lo, mid); r = new Node(mid, hi);
 		}
 		if (mset != inf)
